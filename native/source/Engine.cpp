1fcbb8024fd4014066f625f7473e93fe4a69b715
Aleksas Pielikis <ant.kampo@gmail.com>
2019-12-25 00:12:36 +0200

    Fix str err (#10)
    * Fix str err
    * Version bump

--- native/source/Engine.cpp
+++ native/source/Engine.cpp
@@ -8,7 +8,7 @@
 #include <string.h>
 #include <stdio.h>
 
-char * szEmptyStr = '\0';
+char * szEmptyStr = "\0";
 
 extern "C" {
 
--- phonology_engine/version.py
+++ phonology_engine/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-VERSION = '0.1.14'
+VERSION = '0.1.15'
 RELEASE = '4'
